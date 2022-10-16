b633f0fb66dcbf2cbd3c64e922586e6245476765
David Cuartas <davidcuba91@gmail.com>
2022-10-16 20:15:52 +0200

    Add correct index.js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-// Import getPokemon from "./apiPokemon/getPokemon.js";
-// const newPokemon = await getPokemon("https://pokeapi.co/api/v2/pokemon");
-import Header from "./components/Component/Header/Header";
+import Header from "./components/Component/Header/Header.js";
 const rootElement = document.querySelector(".root");
 const header = new Header(rootElement as HTMLElement);
 header.render();
